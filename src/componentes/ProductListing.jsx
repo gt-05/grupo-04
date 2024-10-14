48ff1c2b8a5bef84c490157c8d0ff499098184ec
Carlos Reiker <carlos.reiker@hotmail.com>
2024-10-14 12:01:52 -0300

    iniciando componente ProductListing

---
+++ src/componentes/ProductListing.jsx
@@ -0,0 +1,9 @@
+export default function ProductListing() {
+    return (
+            <div>
+                <div className="w-[292px] h-[439px]">
+                    
+                </div>
+            </div>     
+    );
+}
