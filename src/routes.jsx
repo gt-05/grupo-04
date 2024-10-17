376f0e80c65c664d3bb3de0cd2bd7fd1714a3696
Reiker <carlos.reiker@hotmail.com>
2024-10-17 11:06:23 -0300

    atualização routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,10 +4,14 @@ import {
         createRoutesFromElements
     } from 'react-router-dom';
 
+    import HomePage from './pages/HomePage';
+    import productListingPage from './pages/ProductListingPage';
+
 export default createBrowserRouter(
     createRoutesFromElements(
          <>
-            <Route path='/' element={(<h1>Rota base</h1>)}/>
+            <Route path='/' element={<HomePage />}/>
+            <Route path='/products' element={<productListingPage />}/>
         </>
     )
 )
